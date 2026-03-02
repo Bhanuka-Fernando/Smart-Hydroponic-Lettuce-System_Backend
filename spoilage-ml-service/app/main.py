50c66ad5ef764e322845609af270c4fbc92f28ea
IT22332158 <154005612+GeshikaThennakoon@users.noreply.github.com>
2026-03-02 14:18:34 +0530

    Implemented the routes for predictions by exact values from the db

--- spoilage-ml-service/app/main.py
+++ spoilage-ml-service/app/main.py
@@ -1,18 +1,29 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router
+from app.db import create_db_and_tables
+from fastapi.staticfiles import StaticFiles
+import os
+
+
+
+# ✅ ensure table model is registered
+import app.models  # noqa
 
 app = FastAPI()
 
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5173",
-        "http://localhost:5173",
-    ],
+    allow_origins=["*"],  # for mobile testing
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+os.makedirs("uploads", exist_ok=True)
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.include_router(router)
